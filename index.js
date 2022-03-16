da7b57aa74805270dc22be968dbd3dae4f29be92
Md Rasedul Islam <rased100@gmail.com>
2022-03-16 22:37:15 +0600

    10 minits

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
+let cron = require('node-cron');
 require('dotenv').config();
 const app = express();
 
@@ -13,33 +14,38 @@ app.get('/', (req, res) => {
     res.send('Node.js Running');
 });
 
+cron.schedule('*/10 * * * *', () => {
+    console.log('running a task every 10 minute');
+    let response = null;
+    new Promise(async (resolve, reject) => {
+        try {
+            response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
+                headers: {
+                    'X-CMC_PRO_API_KEY': `${process.env.API_PASS}`,
+                },
+            });
+        } catch (ex) {
+            response = null;
+            // error
+            console.log(ex);
+            reject(ex);
+        }
+        if (response) {
+            // success
+            const jsonData = response.data;
+            console.log(jsonData);
+            resolve(jsonData);
 
-
-let response = null;
-new Promise(async (resolve, reject) => {
-    try {
-        response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
-            headers: {
-                'X-CMC_PRO_API_KEY': `${process.env.API_PASS}`,
-            },
-        });
-    } catch (ex) {
-        response = null;
-        // error
-        console.log(ex);
-        reject(ex);
-    }
-    if (response) {
-        // success
-        const jsonData = response.data;
-        console.log(jsonData);
-        resolve(jsonData);
-
-        app.get('/jsonData', (req, res) => {
-            res.send(jsonData)
-        });
-    }
+            app.get('/jsonData', (req, res) => {
+                res.send(jsonData)
+            });
+        }
+    });
 });
+
+
+
+
 
 app.listen(port, () => {
     console.log('Listening to port ', port);
