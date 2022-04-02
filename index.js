2ba600d479cfe0eebb69696f915357de6aa073b9
Md Rasedul Islam <rased100@gmail.com>
2022-04-03 00:54:43 +0600

    api done

--- index.js
+++ index.js
@@ -15,34 +15,96 @@ app.get('/', (req, res) => {
     res.send('Node.js Running');
 });
 
-cron.schedule('*/10 * * * *', () => {
-    console.log('running a task every 10 minute');
-    let response = null;
-    new Promise(async (resolve, reject) => {
-        try {
-            response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
-                headers: {
-                    'X-CMC_PRO_API_KEY': `${process.env.API_PASS}`,
-                },
-            });
-        } catch (ex) {
-            response = null;
-            // error
-            console.log(ex);
-            reject(ex);
-        }
-        if (response) {
-            // success
-            const jsonData = response.data;
-            console.log(jsonData);
-            resolve(jsonData);
 
-            app.get('/jsonData', (req, res) => {
-                res.send(jsonData)
-            });
-        }
-    });
+let response = null;
+new Promise(async (resolve, reject) => {
+    try {
+        // response = await axios.get('https://pro-api.coinmarketcap.com/v1/exchange/info', {
+        // response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/info', {
+        // response = await axios.get('https://pro-api.coinmarketcap.com/v2/cryptocurrency/quotes/latest', {
+
+        response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest', {
+            // response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
+            headers: {
+                'X-CMC_PRO_API_KEY': `${process.env.API_PASS}`,
+            },
+        });
+    } catch (ex) {
+        response = null;
+        // error
+        console.log(ex);
+        reject(ex);
+    }
+    if (response) {
+        // success
+        const jsonData = response.data;
+        console.log(jsonData);
+        resolve(jsonData);
+
+        app.get('/jsonData', (req, res) => {
+            res.send(jsonData)
+        });
+    }
 });
+
+
+let response2 = null;
+new Promise(async (resolve, reject) => {
+    try {
+        response2 = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
+            headers: {
+                'X-CMC_PRO_API_KEY': `${process.env.API_PASS}`,
+            },
+        });
+    } catch (ex) {
+        response2 = null;
+        // error
+        console.log(ex);
+        reject(ex);
+    }
+    if (response2) {
+        // success
+        const jsonData2 = response2.data;
+        console.log(jsonData2);
+        resolve(jsonData2);
+
+        app.get('/jsonData2', (req, res) => {
+            res.send(jsonData2)
+        });
+    }
+});
+
+
+// Call api every 10 minutes
+
+// cron.schedule('*/10 * * * *', () => {
+//     console.log('running a task every 10 minute');
+//     let response = null;
+//     new Promise(async (resolve, reject) => {
+//         try {
+//             response = await axios.get('https://pro-api.coinmarketcap.com/v1/cryptocurrency/map', {
+//                 headers: {
+//                     'X-CMC_PRO_API_KEY': `${process.env.API_PASS}`,
+//                 },
+//             });
+//         } catch (ex) {
+//             response = null;
+//             // error
+//             console.log(ex);
+//             reject(ex);
+//         }
+//         if (response) {
+//             // success
+//             const jsonData = response.data;
+//             console.log(jsonData);
+//             resolve(jsonData);
+
+//             app.get('/jsonData', (req, res) => {
+//                 res.send(jsonData)
+//             });
+//         }
+//     });
+// });
 
 
 
